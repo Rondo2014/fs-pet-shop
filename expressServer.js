81fc5211334c50ec825e5e8c31f1be420fe615af
ronnie-miller <jovi@Jovi-Desktop>
2023-04-27 11:54:17 -0700

    Delete feature implemented

--- expressServer.js
+++ expressServer.js
@@ -48,7 +48,6 @@ app.get("/pets/:index", (req, res) => {
 
 app.post("/pets", (req, res) => {
   const newPet = req.body;
-  console.log(newPet);
   if (!newPet || !newPet.name || !newPet.kind || !newPet.age) {
     res.status(400).send("Invalid pet data");
     return;
@@ -71,6 +70,37 @@ app.post("/pets", (req, res) => {
 
       res.setHeader("Content-Type", "application/json");
       res.send(newPet);
+    });
+  });
+});
+
+app.delete("/pets/:index", (req, res) => {
+  const index = parseInt(req.params.index);
+
+  if (isNaN(index)) {
+    res.status(400).send("Invalid index");
+  }
+
+  fs.readFile("pets.json", "utf-8", (err, str) => {
+    if (err) {
+      res.status(500).next();
+    }
+    const pets = JSON.parse(str);
+
+    if (index < 0 || index >= pets.length) {
+      res.status(404).send("Not Found");
+      return;
+    }
+
+    pets.splice(index, 1);
+
+    fs.writeFile("pets.json", JSON.stringify(pets), (err) => {
+      if (err) {
+        res.status(500).next();
+        return;
+      }
+
+      res.status(204).end();
     });
   });
 });
